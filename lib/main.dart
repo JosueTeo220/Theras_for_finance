7bbabcd1d5f351fcda95ec23a8721bb4a2c2432f
unknown <vieiramatheus72@gmail.com>
2023-11-26 01:53:13 -0300

    segundo commit na branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'dart:io';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:theras_app/views/home_screen/main.dart';
+import 'package:theras_app/views/login_screen/main.dart';
 import 'views/menu_empresas/main.dart';
 import 'views/details_screen/main.dart';
 import 'dart:html';
 import 'dart:ui' as ui;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -32,6 +38,10 @@ void inicializarFirebase() async {
 //   runApp(const MyApp());
 // }
 void main() async {
+  await dotenv.load(fileName: ".env");
+  String facebookAppId = dotenv.env[
+          '41692962765-28jgbngm3rpmfv32nsl3j1ku1bckg7q6.apps.googleusercontent.com'] ??
+      '';
   // ignore: undefined_prefixed_name
   ui.platformViewRegistry.registerViewFactory(
     'adViewType',
@@ -69,7 +79,7 @@ class MyApp extends StatelessWidget {
       title: 'Theras for Finance',
       home: FutureBuilder(
         builder: (context, snapshot) {
-          return const MenuEmpresas(title: 'T H Ξ R A S');
+          return HomeScreen();
           // return const MenuEmpresas(title: 'T H Ξ R A S');
         },
         future: null,
